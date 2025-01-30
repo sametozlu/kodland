deed5e675682fc63ee79d6756205dbb54176479b
Halil Samed ÖZLÜ <74491306+sametozlu@users.noreply.github.com>
2025-01-30 12:55:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,25 +32,31 @@ def home():
 def submit():
     name = request.form.get('name')
     session['name'] = name  # Kullanıcı adını oturuma kaydet
-    color = request.form.get('color')
-    animal = request.form.get('animal')
-    hobbies = request.form.get('hobbies')
 
-    # Basit puanlama sistemi
+    # Yeni form elemanları
+    list_answer = request.form.get('list')
+    loop_answer = request.form.get('loop')
+    tuple_vs_list = request.form.get('tuple_vs_list')
+
+    # Basit puanlama sistemi (örnek olarak)
     score = 0
-    if color == 'Blue':
+    if list_answer == 'myList = [1, 2, 3]':
         score += 1
-    if animal == 'Cat':
+    if loop_answer == 'for i in myList:':
         score += 1
-    if hobbies:
-        score += 1
+    if tuple_vs_list:
+        score += 1  # Tuple ve liste farkına dair cevap verildiği için ek puan
 
     # Puanı veritabanına kaydet
     new_score = Score(name=name, score=score)
     db.session.add(new_score)
     db.session.commit()
 
-    return render_template('result.html', name=name, score=score, user_name=name)
+    # En yüksek skoru yeniden sorgula
+    highest_score = Score.query.order_by(Score.score.desc()).first()
+
+    return render_template('result.html', name=name, score=score, user_name=name, highest_score=highest_score)
+
 
 # Hakkında sayfası
 @app.route('/about')
